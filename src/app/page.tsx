d96050958e431b26a2179b6f0335ae8c0426e705
Alex Gichau <gichaumburu@gmail.com>
2025-09-12 07:00:23 +0300

    feat: dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 import * as React from "react"
 import WhatsAppDrawer from "@/components/whatsappDrawer";
 import WeddingCalendar from "@/components/weddingCalendar";
-
-import { useTheme } from "next-themes";
-import {Moon, Sun} from "lucide-react"
+import ThemeBtn from "@/components/darkModeBtn"
 
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+
+      <ThemeBtn/>
       
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         {/* Hero Image */}
